9e3507ddb71bcbb08daa961fd19a16c55a7ae19c
Abolfazl Khodabande <abolfazllkhodabande@gmail.com>
2025-05-14 22:49:03 +0330

    Ui GoldScreen#1

--- app/src/main/java/com/example_app/pricyapp/NavScreen/GoldScreen.kt
+++ app/src/main/java/com/example_app/pricyapp/NavScreen/GoldScreen.kt
@@ -1,6 +1,12 @@
 package com.example_app.pricyapp.NavScreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -11,9 +17,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example_app.pricyapp.mvvm.GoldViewModel
+import java.lang.reflect.Modifier
 
 
 @Composable
@@ -30,20 +39,33 @@ fun GoldScreen(navController: NavHostController) {
         val gold = goldData?.data?.golds ?: emptyList()
 
 
-        LazyColumn {
+        LazyColumn (
+            modifier = androidx.compose.ui.Modifier.fillMaxSize()
+
+        ){
             items(gold) { golds ->
-                Column {
-                    Text(
-                        text = golds.label,
-                        style = MaterialTheme.typography.bodyLarge
-                    )
+                Box(modifier = androidx.compose.ui.Modifier.
+                            fillMaxSize ()
+                    .padding(top = 80.dp)
+                        .height(50.dp) , contentAlignment = Alignment.CenterEnd) {
 
-                    Text(
-                        text = "قیمت: ${golds.price} تومان",
-                        style = MaterialTheme.typography.bodySmall
-                    )
+
+                    Column(
+
+                    ) {
+
+
+                        Text(
+                            text = golds.label,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+
+                        Text(
+                            text = "قیمت: ${golds.price} تومان",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
                 }
-
             }
         }
     } else {
