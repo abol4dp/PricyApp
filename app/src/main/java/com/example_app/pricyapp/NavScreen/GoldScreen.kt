5f89d05240cf2d437b295c01431bd54bcfcd9128
Abolfazl Khodabande <abolfazllkhodabande@gmail.com>
2025-05-20 20:41:15 +0330

    fix CurrencyUi

--- app/src/main/java/com/example_app/pricyapp/NavScreen/GoldScreen.kt
+++ app/src/main/java/com/example_app/pricyapp/NavScreen/GoldScreen.kt
@@ -1,29 +1,20 @@
 package com.example_app.pricyapp.NavScreen
 
-import android.text.Layout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -31,7 +22,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -45,13 +35,16 @@ import com.example_app.pricyapp.ui.theme.smallfontcolor
 
 
 @Composable
+
 fun GoldScreen(navController: NavHostController) {
+
     val viewModel: GoldViewModel = hiltViewModel()
     val goldData by viewModel.goldData.collectAsState()
     val error by viewModel.errorMessage.collectAsState()
 
     LaunchedEffect(Unit) {
         viewModel.fetchGoldData()
+
     }
 
 
